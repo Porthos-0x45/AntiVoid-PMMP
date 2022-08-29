89267c3628736bc9d30780b04be7eaffba60aebd
Null-Pixel <porthossword@gmail.com>
2022-08-29 09:58:25 +0200

    Fixing

--- src/Pixel/AntiVoid/Listener/VoidListener.php
+++ src/Pixel/AntiVoid/Listener/VoidListener.php
@@ -7,7 +7,10 @@ namespace Pixel\AntiVoid\Listener;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerMoveEvent;
 use Pixel\AntiVoid\Core\Main;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\player\PlayerDeathEvent;
 use pocketmine\math\Vector3;
+use pocketmine\player\Player;
 use pocketmine\world\Position;
 use pocketmine\world\World;
 
@@ -17,21 +20,26 @@ class VoidListener implements Listener
     {
     }
 
-    public function onPlayerMove(PlayerMoveEvent $event): void
+    public function onPlayerDamage(EntityDamageEvent $event)
     {
-        $plugin = $this->plugin;
-        $player = $event->getPlayer();
+        $entity = $event->getEntity();
+        $cause = $event->getCause();
 
-        if ($player->getPosition()->y < (float)$plugin->config->getNested("pos")["minY"]) {
-            if (!($plugin->config->getNested("pos")["safe_spawn"])) {
-                $player->teleport(new Position(
-                    (float)$plugin->config->getNested("pos")["x"],
-                    (float)$plugin->config->getNested("pos")["y"],
-                    (float)$plugin->config->getNested("pos")["z"],
-                    $player->getWorld()
-                ));
-            } else {
-                $player->teleport($player->getWorld()->getSafeSpawn());
+        if (($entity instanceof Player) && ($cause == EntityDamageEvent::CAUSE_VOID)) {
+            $player = $event->getEntity();
+            $plugin = $this->plugin;
+
+            if ($player->getPosition()->y < (float)$plugin->config->getNested("pos")["minY"]) {
+                if (!($plugin->config->getNested("pos")["safe_spawn"])) {
+                    $player->teleport(new Position(
+                        (float)$plugin->config->getNested("pos")["x"],
+                        (float)$plugin->config->getNested("pos")["y"],
+                        (float)$plugin->config->getNested("pos")["z"],
+                        $player->getWorld()
+                    ));
+                } else {
+                    $player->teleport($player->getWorld()->getSafeSpawn());
+                }
             }
         }
     }
